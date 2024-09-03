b3b1fb125d0da0329038e0b1c7c4ac8511ea41d7
SimonAlexs <1350966307@qq.com>
2024-09-03 22:07:08 +0800

    饰品图片库适配酒馆战棋30.2.2补丁

--- tests/utils/cut_pictures_util.py
+++ tests/utils/cut_pictures_util.py
@@ -15,8 +15,12 @@ def cut_pictures_in_folder(base_path: str, crop_range: tuple[int,int,int,int], s
 
 
 # 把旅法师营地的图片，裁剪掉白边，只要有效图片
-base_absolute_path = 'D:/Workspace/Git/MyGithub/PictureMatchTool/data/temp/原版图片-未处理/旅法师营地-1024-1024'
-save_folder = 'D:/Workspace/Git/MyGithub/PictureMatchTool/data/database'
-cut_pictures_in_folder(base_absolute_path + '/小饰品', (237, 71, 775, 857), save_folder + '/小饰品')
-cut_pictures_in_folder(base_absolute_path + '/大饰品', (237, 71, 775, 857), save_folder + '/大饰品')
+base_absolute_path = 'E:/WorkSpace/MyGithub/PictureMatchTool/data/temp'
+save_folder = 'E:/WorkSpace/MyGithub/PictureMatchTool/data/database'
+# 509 * 709
+crop_range = (79, 50, 448, 590)
+# 1024 * 1024
+# crop_range = (79, 50, 448, 590)
+cut_pictures_in_folder(base_absolute_path + '/小饰品', crop_range, save_folder + '/小饰品')
+cut_pictures_in_folder(base_absolute_path + '/大饰品', crop_range, save_folder + '/大饰品')
 
